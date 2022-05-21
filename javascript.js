af6821f3177d40cbdaab402da0d8a15ac1325d6c
awo.ali <awoali@hotmail.co.uk>
2022-05-21 16:28:46 +0100

    querySelector display and numbers

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// get element //
+console.log("hello");
+const display = document.querySelector(".grid-container__display")
+const numbers = document.querySelector(".grid-containers__numbers")
+
+//declare (global) variables //
+
+// create function //
+
+
+
+// return function with eventListener //
+
