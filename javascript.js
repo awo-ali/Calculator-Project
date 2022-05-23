a06f5a3254bccba1b450c38b7181721b38ae3799
awo.ali <awoali@hotmail.co.uk>
2022-05-24 00:58:35 +0100

    organising javascript

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-// get elements //
-console.log("hello");
-console.log("world");
+//console.log("hello");
+//console.log("world");
 
 
+// Global Variables 
+
+let firstNumber = ""
+let secondNumber = ""
+let operatorValue = ""
+
+// Get element from HTML
 
 const display = document.querySelector(".grid-container__display");
 const numbers = Array.from(document.getElementsByClassName('grid-container__numbers'));
@@ -11,52 +17,34 @@ const clearAll = document.getElementById('grid-container__clearButton');
 const equals = document.getElementById('grid-container__equalsButton');
 const operators = Array.from(document.getElementsByClassName('grid-container__operator'));
 
+//console.log(display);
+//console.log(numbers);
+//console.log(operators);
 
-let firstNumber = ""
-let secondNumber = ""
-let operatorValue = ""
+// create functions
 
 const showOutput = (event) => {
-  
-  firstNumber = event.target.innerHTML
-  display.innerHTML += firstNumber
-  console.log(display);
+    firstNumber = event.target.innerHTML
+    display.innerHTML += firstNumber
+    console.log(display);
+  };
 
-};
+  const showOperator = (event) => {
+    operatorValue = event.target.innerHTML
+    display.innerHTML += operatorValue
+    console.log(operatorValue);
+  };
 
-numbers.forEach(number => {
- number.addEventListener("click",showOutput )
-});
 
-const showOperator = (event) => {
-  operatorValue = event.target.innerHTML
-  display.innerHTML += operatorValue
-  
-  console.log(operatorValue);
-};
 
 const Del = (event) => {
-  display.innerHTML = display.innerHTML.slice(0, -1);
-};
+    display.innerHTML = display.innerHTML.slice(0, -1);
+  };
 
-deletOne.addEventListener("click", Del)
-
-
-
-operators.forEach(operator => {
-  operator.addEventListener("click",showOutput )
- });
-
- const clearAllButton = (event ) => {
+  
+  const clearAllButton = (event ) => {
     display.innerHTML = "" ;
  };
-
-
-
- clearAll.addEventListener("click", clearAllButton)
-
-
-
 
  /*switch (operatorValue) {
    case '+':
@@ -76,180 +64,72 @@ operators.forEach(operator => {
  } */
 
 
-const sum = () => {
-let secondNumber = firstNumber
-
-//let firstNumber = ''
-  if (operatorValue == '+') {
-     return parseFloat(firstNumber) + parseFloat(secondNumber);
-  }
-  else if (operatorValue == '-'){
-    return firstNumber - secondNumber;
-  }
-  else if (operatorValue == '*'){
-    return firstNumber * secondNumber;
-  }
-  else if (operatorValue == '/'){
-    return firstNumber / secondNumber;
-  }
-
-
-  
-/*let total = 0;
-let firstNumber = secondNumber
-let secondNumber = firstNumber
- switch (operatorValue) {
-
-  case '+':
-    total = parseFloat(firstNumber) + parseFloat(secondNumber)
-   return total;
-  break;
-case '-':
-  result = firstNumber - secondNumber
-  break;
- case '*':
-   result = firstNumber * secondNumber
-   break;
- case '/':
-     result = firstNumber / secondNumber
-     break;
-  default:
-     result = 0*/
+ const sum = () => {
+    let secondNumber = firstNumber
     
-}
-
-//display.innerHTML = total;
-
-// console.log(total);
-
-
-
- equals.addEventListener("click", sum )
-
-
-
- //console.log(operators);
-
- /*let total = (event) => {
-  equals = event.target.innerHTML
-  display.innerHTML += (firstNumber + operators + secondNumber) ;
-  console.log(total);
-};
-
-equals.addEventListener("click", total)
-
-
- // total variable to keep total 
- //update display when equals id pressed
-
-//let Total = 
-
-
-//console.log(firstNumber );
-
-
-
-
-
-/*const newDisplay = (display, numbers) => {
-  return display.innerHTML += numbers.innerHTML;
-  console.log(newDisplay);
-
-};*/
-
-
-
-
-
-
-
-/**/
-
-
-
-
-//returning the function 
-
-
-
-
-//newDisplay.addEventListener('click', numbers);
-
-/*8numbers.forEach((number) => {
-  .onclick = () =>{
-    if ()
-  }
-}) */
-
-
-
-
-
-/*const newDisplay = display.map((display) => {
-    return display.innerHTML += numbers.innerHTML
-})
-//you can only map arrays
-
-console.log(newDisplay);*/
-
-/*numbers.map(number => {
-    number.addEventListener(`click`, (Event) =>{
-        switch(Event.target.innerHTML) {
-            default:
-                display.innerHTML =+ Event.target.innerHTML;
-        }
-    })
-}); */
-
-
-//const numberDisplay = ((numbers) => {
-    //console.log(`display.innerText += ${numbers}`);
-//})
-//numbers.forEach((number) => {
-    //return number =+ display;
-//})
-
-//const numberDisplay = numbers.map((number) => {
-  //  return display.innerText += number
-//})
-//console.log(numberDisplay);
-
-
-//const newDisplay = display.foreach (display.innerHTML += numbers.innerHTML)
-
-//const newDisplay = display.map(display += numbers);
-//const numberDisplay = numbers.forEach(number) 
-  //  number =+ display;
-
-
-//numbers.forEach(number => {
-   // (display.innerText =+ numbers.innertext);
+    //let firstNumber = ''
+      if (operatorValue == '+') {
+         return parseFloat(firstNumber) + parseFloat(secondNumber);
+      }
+      else if (operatorValue == '-'){
+        return firstNumber - secondNumber;
+      }
+      else if (operatorValue == '*'){
+        return firstNumber * secondNumber;
+      }
+      else if (operatorValue == '/'){
+        return firstNumber / secondNumber;
+      }
     
-//});
- //numbers.forEach(display.innerText =+ numbers.innertext);
+    
+      equals.addEventListener("click", sum )
+      
+    /*let total = 0;
+    let firstNumber = secondNumber
+    let secondNumber = firstNumber
+     switch (operatorValue) {
+    
+      case '+':
+        total = parseFloat(firstNumber) + parseFloat(secondNumber)
+       return total;
+      break;
+    case '-':
+      result = firstNumber - secondNumber
+      break;
+     case '*':
+       result = firstNumber * secondNumber
+       break;
+     case '/':
+         result = firstNumber / secondNumber
+         break;
+      default:
+         result = 0*/
+        
+    }
+    
+    //display.innerHTML = total;
+    
+    // console.log(total);
+    
 
-// functions //
-
-/*switch(display) {
- case (numbers):
-  display += numbers
-
-} */
-//
 
 
+// return function with eventListener  
+
+numbers.forEach(number => {
+    number.addEventListener("click",showOutput )
+   });
 
 
+   operators.forEach(operator => {
+    operator.addEventListener("click",showOutput )
+   });
 
 
-//declare (global) variables //
+deletOne.addEventListener("click", Del)
 
 
+clearAll.addEventListener("click", clearAllButton)
 
 
-// create function //
-
-
-
-// return function with eventListener //
 
